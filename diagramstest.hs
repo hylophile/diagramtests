6aebe9415979b61ef6e689056965d0f2b85a453f
Tani Cat <anon008@inbox.com>
2015-05-03 05:25:11 +0200

    added magicConstant

--- diagramstest.hs
+++ diagramstest.hs
@@ -7,12 +7,15 @@ main = mainWith d
 
 {-example = hcat (map circle (iterate (/1.618) 1))-}
 {-example = hcat (circle  1 )-}
+phi = 1.618
+e = 2.718
+magicConstant = e
 
 {-d :: Diagram SVG V2 Double-}
 d :: Diagram B
 d = (mconcat $ take 8 $ iterate (rotateBy (1/8)) c)
 c:: Diagram B
-c = hcat $ invisHead (map d' (take 7 (iterate (/1.618) 1)))
+c = hcat $ invisHead (map d' (take 7 (iterate (/magicConstant) 1)))
 
 invisHead (x:xs) = opacity 0 x : xs
 invisHead [] = []
@@ -21,4 +24,4 @@ invisHead [] = []
 d' :: Double -> Diagram B
 d' r = scale r $ mconcat $ take 6 $ iterate (rotateBy (1/6)) c'
 c' :: Diagram B
-c' = hcat $ (map circle (take 4 (iterate (/1.618) 1)))
+c' = hcat $ (map circle (take 4 (iterate (/magicConstant) 1)))
